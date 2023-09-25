494b500cabd5173f767f7fc751518eb2b3042f6a
Ashwanth <ashwanthbalakrishnan5@gmail.com>
2023-09-26 00:48:39 +0530

    update animals model

--- api/models.py
+++ api/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Animals(models.Model):
     name = models.CharField(max_length=100)
-    short_description = models.TextField()
     image = models.ImageField(upload_to="images/")
 
     def __str__(self):
