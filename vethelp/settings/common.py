06afd01dec3d41440fe27bb23046f73fa1cd0fd2
Ashwanth <ashwanthbalakrishnan5@gmail.com>
2023-09-26 00:44:45 +0530

    update static files root

--- vethelp/settings/common.py
+++ vethelp/settings/common.py
@@ -121,8 +121,11 @@ AWS_DEFAULT_ACL = "private"
 
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
