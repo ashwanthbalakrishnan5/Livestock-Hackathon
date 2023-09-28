7c7e66fdaea28165e2fe5a885c24637c5e222346
Ashwanth <ashwanthbalakrishnan5@gmail.com>
2023-09-28 19:58:07 +0530

    removed aws storage

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Animals(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="images/")
+    image = models.URLField()
 
     def __str__(self):
         return self.name
--- vethelp/settings/common.py
+++ vethelp/settings/common.py
@@ -113,22 +113,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-AWS_ACCESS_KEY_ID = "AKIAXRMBPSOWU6TNJ7T6"
-AWS_SECRET_ACCESS_KEY = "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"
-AWS_STORAGE_BUCKET_NAME = "livestockhackathon1"
-AWS_S3_REGION_NAME = "ap-south-1"
-AWS_DEFAULT_ACL = "private"
-
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-
-# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-# STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
